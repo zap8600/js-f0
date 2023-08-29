78362d36f3f6a65a3cb56d8f92896113174cf553
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-08-29 10:43:52 -0500

    code

--- js.c
+++ js.c
@@ -5,36 +5,23 @@
 #include <furi.h>
 #include <gui/gui.h>
 #include <storage/storage.h>
-//#include <toolbox/stream/stream.h>
-//#include <toolbox/stream/file_stream.h>
 
 #include "microvium.h"
-
-//#include "script.mvm-bc.h"
 
 // A function in the host (this file) for the VM to call
 #define IMPORT_PRINT 1
 
 // A function exported by VM to for the host to call
-const mvm_VMExportID MAIN = 1234;
+const mvm_VMExportID INIT = 1;
 
 mvm_TeError resolveImport(mvm_HostFunctionID id, void*, mvm_TfHostFunction* out);
-mvm_TeError print(mvm_VM* vm, mvm_HostFunctionID funcID, mvm_Value* result, mvm_Value* args, uint8_t argCount);
-
-/*
-typedef struct {
-    FuriThread* thread;
-} JSThread;
-*/
+mvm_TeError console_log(mvm_VM* vm, mvm_HostFunctionID funcID, mvm_Value* result, mvm_Value* args, uint8_t argCount);
 
 FuriString* conLog;
 int conY;
 int conX;
 
-//JSThread* main_js_thread;
-
-//InputEvent event;
-//FuriMessageQueue* event_queue;
+ViewPort* view_port;
 
 static void draw_callback(Canvas* canvas, void* context) {
     UNUSED(context);
@@ -51,7 +38,7 @@ static void input_callback(InputEvent* input_event, void* ctx) {
 int32_t js_run(uint8_t* fileBuff, size_t fileSize) {
     mvm_TeError err;
     mvm_VM* vm;
-    mvm_Value main;
+    mvm_Value init;
     mvm_Value result;
 
     // Restore the VM from the snapshot
@@ -62,14 +49,14 @@ int32_t js_run(uint8_t* fileBuff, size_t fileSize) {
     }
 
     // Find the "sayHello" function exported by the VM
-    err = mvm_resolveExports(vm, &MAIN, &main, 1);
+    err = mvm_resolveExports(vm, &INIT, &init, 1);
     if (err != MVM_E_SUCCESS) {
         FURI_LOG_E("microvium", "Error with exports: %d", err);
         return err;
     }
 
     // Call "sayHello"
-    err = mvm_call(vm, main, &result, NULL, 0);
+    err = mvm_call(vm, init, &result, NULL, 0);
     if (err != MVM_E_SUCCESS) {
         FURI_LOG_E("microvium", "Error with call: %d", err);
         return err;
@@ -81,22 +68,7 @@ int32_t js_run(uint8_t* fileBuff, size_t fileSize) {
     return 0;
 }
 
-/*
-static void configure_and_start_thread(int funcID) {
-    UNUSED(funcID); //kept for future use
-    main_js_thread->thread = furi_thread_alloc();
-    // main_js_thread->FUNCID = funcID;
-    furi_thread_set_name(main_js_thread->thread, "Microvium");
-    furi_thread_set_stack_size(main_js_thread->thread, 1024);
-    furi_thread_set_context(main_js_thread->thread, main_js_thread);
-    furi_thread_set_callback(main_js_thread->thread, js_thread_body);
-    furi_thread_start(main_js_thread->thread);
-    return;
-}
-*/
-
 int32_t js_app() {
-    //size_t fileSize;
     InputEvent event;
     FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
 
@@ -113,42 +85,23 @@ int32_t js_app() {
 
     furi_record_close(RECORD_STORAGE);
 
-    ViewPort* view_port = view_port_alloc();
+    view_port = view_port_alloc();
     view_port_draw_callback_set(view_port, draw_callback, NULL);
     view_port_input_callback_set(view_port, input_callback, event_queue);
 
     Gui* gui = furi_record_open(RECORD_GUI);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    /*
-    Storage* storage = furi_record_open(RECORD_STORAGE);
-    File* bytecode = storage_file_alloc(storage);
-    storage_file_open(bytecode, EXT_PATH("script.mvm-bc"), FSAM_READ, FSOM_OPEN_EXISTING);
-    Stream* stream = file_stream_alloc(storage);
-    file_stream_open(stream, "/ext/script.mvm-bc", FSAM_READ, FSOM_OPEN_EXISTING);
-    fileSize = stream_size(stream);
-    file_stream_close(stream);
-    stream_free(stream);
-    */
-
     conLog = furi_string_alloc();
 
     while(1) {
         furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
         if(event.key == InputKeyOk) {
-            js_run(fileBuff, fileSize); //storage_file_size(bytecode));
+            js_run(fileBuff, fileSize);
             break;
         }
     }
     furi_delay_ms(500);
-
-    /*
-    furi_thread_join(main_js_thread->thread);
-
-    furi_thread_free(main_js_thread->thread);
-
-    free(main_js_thread);
-    */
 
     while(1) {
         furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
@@ -158,13 +111,6 @@ int32_t js_app() {
     }
 
     furi_string_free(conLog);
-
-    /*
-    storage_file_close(bytecode);
-    storage_file_free(bytecode);
-
-    furi_record_close(RECORD_STORAGE);
-    */
 
     furi_message_queue_free(event_queue);
     gui_remove_view_port(gui, view_port);
@@ -193,12 +139,21 @@ mvm_TeError resolveImport(mvm_HostFunctionID funcID, void* context, mvm_TfHostFu
     return MVM_E_UNRESOLVED_IMPORT;
 }
 
-mvm_TeError print(mvm_VM* vm, mvm_HostFunctionID funcID, mvm_Value* result, mvm_Value* args, uint8_t argCount) {
+mvm_TeError console_log(mvm_VM* vm, mvm_HostFunctionID funcID, mvm_Value* result, mvm_Value* args, uint8_t argCount) {
     UNUSED(funcID);
     UNUSED(result);
     furi_assert(argCount == 3);
     furi_string_printf(conLog, "%s\n", (const char*)mvm_toStringUtf8(vm, args[0], NULL));
     conX = mvm_toInt32(vm, args[1]);
     conY = mvm_toInt32(vm, args[2]);
+    view_port_update(view_port);
     return MVM_E_SUCCESS;
+}
+
+mvm_TeError console_clear(mvm_VM* vm, mvm_HostFunctionID funcID, mvm_Value* result, mvm_Value* args, uint8_t argCount) {
+    UNUSED(funcID);
+    UNUSED(result);
+    UNUSED(args);
+    furi_assert(argCount == 0);
+    canvas
 }
